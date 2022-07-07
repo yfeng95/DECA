022ed525f117a81a0e9338b0118d5191770982be
YadiraF <yao.feng@tuebingen.mpg.de>
2022-07-08 01:29:29 +0200

    add script for downloading data

--- README.md
+++ README.md
@@ -41,9 +41,13 @@ Clone the repo:
 
 ### Usage
 1. Prepare data   
-    a. download [FLAME model](https://flame.is.tue.mpg.de/download.php), choose **FLAME 2020** and unzip it, copy 'generic_model.pkl' into ./data  
-    b. download [DECA trained model](https://drive.google.com/file/d/1rp8kdyLPvErw2dTmqtjISRVvQLj6Yzje/view?usp=sharing), and put it in ./data (**no unzip required**)  
-    c. (Optional) follow the instructions for the [Albedo model](https://github.com/TimoBolkart/BFM_to_FLAME) to get 'FLAME_albedo_from_BFM.npz', put it into ./data
+    run script: 
+    ```bash
+    bash fetch_data.sh
+    ```
+    <!-- or manually download data form [FLAME 2020 model](https://flame.is.tue.mpg.de/download.php) and [DECA trained model](https://drive.google.com/file/d/1rp8kdyLPvErw2dTmqtjISRVvQLj6Yzje/view?usp=sharing), and put them in ./data  -->  
+    (Optional for Albedo)   
+    follow the instructions for the [Albedo model](https://github.com/TimoBolkart/BFM_to_FLAME) to get 'FLAME_albedo_from_BFM.npz', put it into ./data
 
 2. Run demos  
     a. **reconstruction**  
---
+++ fetch_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+urle () { [[ "${1}" ]] || return 1; local LANG=C i x; for (( i = 0; i < ${#1}; i++ )); do x="${1:i:1}"; [[ "${x}" == [a-zA-Z0-9.~-] ]] && echo -n "${x}" || printf '%%%02X' "'${x}"; done; echo; }
+
+# Fetch FLAME data
+echo -e "\nBefore you continue, you must register at https://flame.is.tue.mpg.de/ and agree to the FLAME license terms."
+read -p "Username (FLAME):" username
+read -p "Password (FLAME):" password
+username=$(urle $username)
+password=$(urle $password)
+
+mkdir -p ./data
+
+echo -e "\nDownloading FLAME..."
+wget --post-data "username=$username&password=$password" 'https://download.is.tue.mpg.de/download.php?domain=flame&sfile=FLAME2020.zip&resume=1' -O './data/FLAME2020.zip' --no-check-certificate --continue
+unzip ./data/FLAME2020.zip -d ./data/FLAME2020
+mv ./data/FLAME2020/generic_model.pkl ./data
+# rm -rf ./models
+
+echo -e "\nDownloading deca_model..."
+
+FILEID=1rp8kdyLPvErw2dTmqtjISRVvQLj6Yzje
+FILENAME=./data/deca_model.tar
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='${FILEID} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILEID}" -O $FILENAME && rm -rf /tmp/cookies.txt
